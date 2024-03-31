b2af7492ca90c69b2a961b3ed539e8bea910b44f
Aya <ayaelrhayour123@gmail.com>
2024-03-31 17:08:10 +0000

    added a protected route for the topic

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\TopicController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TopicController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +16,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/login', [AuthController::class, 'login']);
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('topics', TopicController::class);
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::apiResource('topics', TopicController::class)->middleware('restrictRole:Admin');
+});
+
