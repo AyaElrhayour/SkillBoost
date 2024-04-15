7aecfbd4d68ec6d8fb7b1ddc36c6942608f265a9
Aya <ayaelrhayour123@gmail.com>
2024-04-15 16:47:17 +0100

    updating kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,9 +39,14 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+
+            'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            'auth:sanctum',
+            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            // \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            // \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
 
