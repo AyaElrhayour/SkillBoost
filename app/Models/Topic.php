dc4e49486ebe20e63b85ddd79d631228cc93d9fa
Aya <ayaelrhayour123@gmail.com>
2024-04-15 16:39:20 +0100

    adding topic model

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Topic extends Model
 {
@@ -17,4 +18,9 @@ class Topic extends Model
     protected $hidden =[
         'updated_at',  
     ];
+
+    public function courses(): HasMany 
+    {
+        return $this->hasMany(Course::class, 'user_id');
+    }
 }
