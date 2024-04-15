aeab6b3168a4f65939870daf53ffd51f08ece6fb
Aya <ayaelrhayour123@gmail.com>
2024-04-15 16:52:46 +0100

    updating api routes

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\TopicController;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\ChapterController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\ChapterStudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,14 +23,29 @@ use App\Http\Controllers\TopicController;
 
 Route::post('/auth/register', [AuthController::class, 'register']);
 Route::post('/auth/login', [AuthController::class, 'login']);
-
+Route::post('/logout', [AuthController::class, 'logout']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::apiResource('courses', CourseController::class);
 
-Route::middleware('auth:sanctum')->group(function(){
-    Route::apiResource('topics', TopicController::class)->middleware('restrictRole:Admin');
+Route::apiResource('chapters', ChapterController::class);
+
+Route::apiResource('chapterstudent', ChapterStudentController::class);
+
+Route::apiResource('posts', PostController::class);
+
+Route::apiResource('comments', CommentController::class);
+
+Route::get('topics', [TopicController::class, 'index']);
+
+// Route::apiResources([
+//     "courses" => CourseController::class,
+
+// ]);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('topics', TopicController::class)->middleware('restrictRole:Admin')->except('index');
 });
 
