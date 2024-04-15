52c842f6b5308418c89b178630dfd8efdc05619a
Aya <ayaelrhayour123@gmail.com>
2024-04-15 16:36:41 +0100

    adding a course model

---
+++ app/Models/Course.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Course extends Model
+{
+    use HasFactory;
+    protected $table = "courses";
+
+    protected $fillable = [
+        'title',
+        'description',
+        'level',
+        'cover',
+        'topic_id',
+        'user_id',
+    ];
+
+    protected $hidden = [
+        'updated_at',
+    ];
+
+    public function teacher(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function topic(): BelongsTo
+    {
+        return $this->belongsTo(Topic::class, 'topic_id');
+    }
+
+    protected static function booted(): void{
+
+        static::addGlobalScope('user', function (Builder $builder) {
+            $builder->where('user_id', Auth::id());
+        });
+    }
+}
