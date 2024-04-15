4b52cef87ad57374f6b871e386c921c48055eb14
Aya <ayaelrhayour123@gmail.com>
2024-04-15 16:41:32 +0100

    adding chapter model

---
+++ app/Models/Chapter.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Chapter extends Model
+{
+    use HasFactory;
+
+    protected $table = "chapter";
+
+    protected $fillable = [
+        'title',
+        'content',
+        'type',
+        'course_id',
+    ];
+
+    protected $hidden = [
+        'updated_at',
+    ];
+
+    public function teacher(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function course(): BelongsTo
+    {
+        return $this->belongsTo(Course::class, 'topic_id');
+    }
+
+    protected static function booted(): void{
+
+        static::addGlobalScope('user', function (Builder $builder) {
+            $builder->where('user_id', Auth::id());
+        });
+    }
+}
