ed422cfc7223af6b0aa73f683876307dcea01fac
Aya <ayaelrhayour123@gmail.com>
2024-04-17 19:45:27 +0100

    adding new component

---
+++ frontend/src/components/Description.jsx
@@ -0,0 +1,45 @@
+import calander from "../assets/calander.png";
+import people from "../assets/people.png";
+import document from "../assets/document.png";
+import DescriptionCard from "../shared/MyCard";
+
+const Description = () => {
+  return (
+    <div className="flex flex-col my-8 gap-6">
+      <div className="flex flex-col items-center gap-6">
+        <h2 className="text-3xl font-bold text-[#001D6E]">
+          All-In-One Cloud Software.
+        </h2>
+        <p className="text-xl text-wrap w-1/2 text-center text-[#696984]">
+          SkillBoost is one powerful online software suite that combines all the
+          tools needed to run a successful school or office.
+        </p>
+      </div>
+      <div className="grid grid-cols-3 gap-16 mx-20 justify-items-center">
+        <DescriptionCard
+          title={" Online Billing, Invoicing, & Contracts"}
+          description={
+            "Simple and secure control of your organization’s financial and legal transactions. Send customized invoices and contracts"
+          }
+          document={document}
+        />
+
+        <DescriptionCard
+          document={calander}
+          title={"Easy Scheduling & Attendance Tracking"}
+          description={
+            "Schedule and reserve classrooms at one campus or multiple campuses. Keep detailed records of student attendance"
+          }
+        />
+        <DescriptionCard
+          document={people}
+          title={"Customer Tracking"}
+          description={
+            "Automate and track emails to individuals or groups. Skilline’s built-in system helps organize your organization"
+          }
+        />
+      </div>
+    </div>
+  );
+};
+export default Description;
