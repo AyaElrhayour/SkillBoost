faaf42935fead6e807212e27e6dbfad63b121763
Aya <ayaelrhayour123@gmail.com>
2024-04-17 19:44:39 +0100

    adding new props

---
+++ frontend/src/shared/MyCard.jsx
@@ -0,0 +1,15 @@
+const DescriptionCard = ({ document, title, description }) => {
+  return (
+    <div className="flex flex-col items-center w-2/3 rounded-[10px] [box-shadow:_0.3em_0.3em_1em_rgba(0,0,0,0.1)] px-4 py-6">
+      <img src={document} alt="" />
+      <div className="">
+        <h3 className="text-[#2F327D] text-lg font-semibold text-center">
+          {title}
+        </h3>
+        <p className="text-[#696984] text-center ">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionCard;
