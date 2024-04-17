feef96b4c9c47c16c9920971887ec8d23934999f
Aya <ayaelrhayour123@gmail.com>
2024-04-17 19:46:58 +0100

    updating the homeand i am  done with the 1st page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,17 @@
+import Description from "../components/Description";
 import Hero from "../components/Hero";
+import Introduction from "../components/Introduction";
+import OurFeatures from "../components/OurFeatures";
+import Footer from "../shared/Footer";
 
 const Home = () => {
   return (
-    <div className="w-full">
+    <div className="w-full flex flex-col gap-16">
       <Hero />
+      <Description />
+      <Introduction />
+      <OurFeatures />
+      <Footer/>
     </div>
   );
 };
