841d2f6fe1a6c1aad44661d8da50fb5ab0154720
Aya <ayaelrhayour123@gmail.com>
2024-04-17 19:45:57 +0100

    feature component

---
+++ frontend/src/components/OurFeatures.jsx
@@ -0,0 +1,12 @@
+import OurFeature from "../assets/OurFeature.png";
+
+
+const OurFeatures = () => {
+  return(
+<div className="flex justify-center" >
+  <img src={OurFeature} alt="" />
+</div>
+
+  );
+};
+export default OurFeatures;
