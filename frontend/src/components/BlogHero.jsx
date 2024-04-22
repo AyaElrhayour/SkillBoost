a952ec8415b56a7c6d889e58e4037f0ecfa53f97
Aya <ayaelrhayour123@gmail.com>
2024-04-22 16:16:43 +0100

    adding a blog hero section

---
+++ frontend/src/components/BlogHero.jsx
@@ -0,0 +1,9 @@
+import Hero from "../assets/bloghero.png"
+const BlogHero = () =>{
+  return(
+    <div>
+      <img src={Hero} alt="" className="w-full" />
+    </div>
+  )
+}
+export default BlogHero;
