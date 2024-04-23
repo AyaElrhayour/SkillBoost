f5da6bdbc368e758080438c1c7b9844829b68a92
Aya <ayaelrhayour123@gmail.com>
2024-04-23 09:13:58 +0100

    adding onlib=ne courses section

---
+++ frontend/src/components/CourseOnline.jsx
@@ -0,0 +1,9 @@
+import Online from "../assets/online.png";
+const CourseOnline = () => {
+  return (
+    <div className="self-center mb-8">
+      <img src={Online} alt="" className="w-full" />
+    </div>
+  );
+};
+export default CourseOnline;
