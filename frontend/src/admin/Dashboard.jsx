4839e013f57dfee5b38011008c1912ee2f16a857
Aya <ayaelrhayour123@gmail.com>
2024-04-23 09:32:59 +0100

    adding an admin folder

---
+++ frontend/src/admin/Dashboard.jsx
@@ -0,0 +1,18 @@
+import CardPageVisits from "../shared/Cards/CardPageVisits";
+import CardSocialTraffic from "../shared/Cards/CardSocialTraffic";
+
+const Dashboard = () => {
+  return (
+    <>
+      <div className="flex flex-wrap mt-4">
+        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
+          <CardPageVisits />
+        </div>
+        <div className="w-full xl:w-4/12 px-4">
+          <CardSocialTraffic />
+        </div>
+      </div>
+    </>
+  );
+}
+export default Dashboard;
