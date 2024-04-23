2588bd5bff885277c14bc8baf0a3f474fdc8f92c
Aya <ayaelrhayour123@gmail.com>
2024-04-23 09:22:01 +0100

    the navbar golabal component

--- frontend/src/shared/Navigation.jsx
+++ frontend/src/shared/Navigation.jsx
@@ -1,23 +1,35 @@
-import white_logo from "../assets/white_logo.png";
 import Button from "./Button";
+import white_logo from "../assets/white_logo.png";
+import Account from "./Account";
 
-const Navigation = () => {
+
+const Navigation = ({
+  textColor = "text-white",
+  logoSrc = white_logo,
+  showButtons = true,
+  margin = "ml-48"
+}) => {
   return (
     <nav>
-      <div className="flex  justify-around ml-48 items-center">
-        <ul className="flex  gap-20 items-center text-white text-lg font-semibold">
+      <div className={`flex justify-between items-center ${margin}`}>
+        <ul className={`flex gap-20 items-center text-lg font-semibold ${textColor}`}>
           <li>Home</li>
           <li>Course</li>
           <li>
-            <img src={white_logo} alt="" className="w-20" />
+            <img src={logoSrc} alt="Logo" className="w-20" />
           </li>
           <li>Blog</li>
           <li>About Us</li>
         </ul>
-        <div className="flex justify-end gap-8 ml-28">
-          <Button content={"Log In"} />
-          <Button bgColor="bg-[#A5CAFE]" content={"Sign Up"} />
-        </div>
+        {showButtons ? (
+          <div className="flex justify-end gap-8 ml-28">
+            <Button content={"Log In"} />
+            <Button bgColor="bg-[#A5CAFE]" content={"Sign Up"} />
+          </div>
+        ) : (
+          
+          <Account/>
+        )}
       </div>
     </nav>
   );
